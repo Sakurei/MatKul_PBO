7be5dd68457bd316b01639aa6e55144f2f937b1f
Danz <mikael.eureka@gmail.com>
2026-03-06 13:23:56 +0700

    penambahan setter dan getter

--- pbo_enkapsulasi/Produk.java
+++ pbo_enkapsulasi/Produk.java
@@ -10,4 +10,50 @@ package com.mycompany.pbo_enkapsulasi;
  */
 public class Produk {
     
+    private int id;
+    private String name;
+    private int stock;
+    private double price;
+    private String category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    
 }
