bd150ef236509c1fcab804ca8adb6515c6c6379e
Danz <mikael.eureka@gmail.com>
2026-03-06 13:02:53 +0700

    first commit

---
+++ pbo_enkapsulasi/PBO_Enkapsulasi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo_enkapsulasi;
+
+/**
+ *
+ * @author User
+ */
+public class PBO_Enkapsulasi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
